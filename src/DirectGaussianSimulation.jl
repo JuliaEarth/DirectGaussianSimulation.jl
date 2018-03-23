b7bae242c18da54aeedf99839108755e83eacf16
Júlio Hoffimann <julio.hoffimann@gmail.com>
2018-03-22 20:41:11 -0700

    Add initial code for the solver

--- src/DirectGaussianSimulation.jl
+++ src/DirectGaussianSimulation.jl
@@ -1,6 +1,36 @@
+# ------------------------------------------------------------------
+# Copyright (c) 2018, Júlio Hoffimann Mendes <juliohm@stanford.edu>
+# Licensed under the ISC License. See LICENCE in the project root.
+# ------------------------------------------------------------------
+
 __precompile__()
+
 module DirectGaussianSimulation
 
-# Package code goes here.
+importall GeoStatsBase
+using GeoStatsDevTools
+
+"""
+    DirectGaussSim(var₁=>param₁, var₂=>param₂, ...)
+
+Direct Gaussian simulation (a.k.a. LU simulation).
+
+## Parameters
+
+* `variogram` - theoretical variogram (default to GaussianVariogram())
+
+### References
+
+Alabert 1987. *The practice of fast conditional simulations through the
+LU decomposition of the covariance matrix.*
+"""
+@simsolver DirectGaussSim begin
+  @param variogram = GaussianVariogram()
+end
+
+function solve(problem::SimulationProblem, solver::DirectGaussSim)
+end
+
+export DirectGaussSim
 
 end
