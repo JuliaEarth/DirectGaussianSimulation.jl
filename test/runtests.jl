381347688262cbd8c7710e6d5c5e3db1c826901f
Júlio Hoffimann <julio.hoffimann@gmail.com>
2018-04-07 15:44:42 -0700

    Add tests for unconditional simulation

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,23 +16,44 @@ ismaintainer = "USER" ∈ keys(ENV) && ENV["USER"] ∈ maintainers
 istravislinux = "TRAVIS" ∈ keys(ENV) && ENV["TRAVIS_OS_NAME"] == "linux"
 datadir = joinpath(@__DIR__,"data")
 
-@testset "Basic problem" begin
+@testset "DirectGaussianSimulation.jl" begin
   geodata = GeoDataFrame(DataFrames.DataFrame(x=[0.,25.,50.,75.,100.], variable=[0.,1.,0.,1.,0.]), [:x])
   domain = RegularGrid{Float64}(100)
-  problem = SimulationProblem(geodata, domain, :variable, 3)
+  @testset "Conditional simulation" begin
+    problem = SimulationProblem(geodata, domain, :variable, 3)
 
-  srand(2018)
-  solver = DirectGaussSim(:variable => @NT(variogram=SphericalVariogram(range=10.)))
+    srand(2018)
+    solver = DirectGaussSim(:variable => @NT(variogram=SphericalVariogram(range=10.)))
 
-  solution = solve(problem, solver)
+    solution = solve(problem, solver)
 
-  if ismaintainer || istravislinux
-    function plot_solution(fname)
-      plot(solution, size=(1000,400))
-      png(fname)
+    if ismaintainer || istravislinux
+      function plot_cond_solution(fname)
+        plot(solution, size=(1000,400))
+        png(fname)
+      end
+      refimg = joinpath(datadir,"CondSimSol.png")
+
+      @test test_images(VisualTest(plot_cond_solution, refimg), popup=!istravislinux) |> success
     end
-    refimg = joinpath(datadir,"solution.png")
+  end
 
-    @test test_images(VisualTest(plot_solution, refimg), popup=!istravislinux) |> success
+  @testset "Unconditional simulation" begin
+    problem = SimulationProblem(domain, :variable => Float64, 3)
+
+    srand(2018)
+    solver = DirectGaussSim(:variable => @NT(variogram=SphericalVariogram(range=10.)))
+
+    solution = solve(problem, solver)
+
+    if ismaintainer || istravislinux
+      function plot_uncond_solution(fname)
+        plot(solution, size=(1000,400))
+        png(fname)
+      end
+      refimg = joinpath(datadir,"UncondSimSol.png")
+
+      @test test_images(VisualTest(plot_uncond_solution, refimg), popup=!istravislinux) |> success
+    end
   end
 end
